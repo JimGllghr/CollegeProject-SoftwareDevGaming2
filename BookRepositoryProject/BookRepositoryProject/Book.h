b5c8b85ca4ff1055d39cd3ad5d600567277f8e3d
unknown <A00209091@X207-HP-20.studentnet.ait.ie>
2015-10-22 09:22:34 +0100

    Added Project, Book.h, book.cpp [JG]

---
+++ BookRepositoryProject/BookRepositoryProject/Book.h
@@ -0,0 +1,35 @@
+#ifndef nbook
+#define nbook
+#include <string>
+
+#include <iostream>
+
+class book
+{
+public:
+	std::string getAuthor() const;
+	std::string getTitle() const;
+	unsigned int getISBN() const;
+
+	void setBook(std::string nAuthor, std::string nTitle, unsigned int nISBN, book* nextParam, book* prevParam);
+
+	void setAuthor(std::string nAuthor);
+	void setTitle(std::string ntitle);
+	void setISBN(unsigned int nISBN);
+
+	void setNext(book* nextParam);
+	void setPrev(book* prevParam);
+
+	book* getNext() const;
+	book* getPrev() const;
+
+private:
+	std::string author;
+	std::string title;
+	unsigned int ISBN;
+
+	book* next;
+	book* prev;
+};
+
+#endif
---
+++ BookRepositoryProject/BookRepositoryProject/book.cpp
@@ -0,0 +1,60 @@
+#include "book.h";
+
+void book::setAuthor(std::string nAuthor)
+{
+	author = nAuthor;
+}
+
+void book::setTitle(std::string nTitle)
+{
+	title = nTitle;
+}
+
+void book::setISBN(unsigned int nISBN)
+{
+	ISBN = nISBN;
+}
+
+std::string book::getAuthor() const
+{
+	return author;
+}
+
+std::string book::getTitle() const
+{
+	return title;
+}
+
+unsigned int book::getISBN() const
+{
+	return ISBN;
+}
+
+void book::setNext(book* nextParam)
+{
+	next = nextParam;
+}
+
+void book::setPrev(book* prevParam)
+{
+	prev = prevParam;
+}
+
+book* book::getNext() const
+{
+	return next;
+}
+
+book* book::getPrev() const
+{
+	return prev;
+}
+
+void book::setAuthor(std::string nAuthor, std::string nTitle, unsigned int nISBN, book* nextParam, book* prevParam)
+{
+	author = nAuthor;
+	title = nTitle;
+	ISBN = nISBN;
+	next = nextParam;
+	prev = prevParam;
+}
